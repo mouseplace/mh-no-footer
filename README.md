db8a03f3857fb32853ca6ecb5ebe584ef7a0e9f2
Brad Parbs <brad@bradparbs.com>
2024-01-17 23:19:37 -0600

    Add script migration notice

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A Userscript for [MouseHunt](https://mousehuntgame.com) to hide the footer.
 
+> [!TIP]
+> You should install [MouseHunt Improved](https://github.com/MHCommunity/mousehunt-improved) instead of this script, as it includes this functionality and tons more!
+
 ## Installation
 
 *You must have [ViolentMonkey](https://violentmonkey.github.io/) or another userscript manager installed.*
--- mh-no-footer.user.js
+++ mh-no-footer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ MouseHunt - No Footer
-// @version      1.0.0
+// @version      1.1.0
 // @description  Hides the footer.
 // @license      MIT
 // @author       bradp
@@ -9,6 +9,7 @@
 // @icon         https://i.mouse.rip/mouse.png
 // @grant        none
 // @run-at       document-end
+// @require      https://cdn.jsdelivr.net/npm/script-migration@1.1.1
 // ==/UserScript==
 
 ((function () {
@@ -17,4 +18,6 @@
   const addStyles = document.createElement('style');
   addStyles.innerHTML = '.pageFrameView-footer, .mousehuntFooter { display: none; }';
   document.head.appendChild(addStyles);
+
+  migrateUserscript('🐭️ MouseHunt - No Footer', 'https://greasyfork.org/en/users/921385-bradp');
 })());
